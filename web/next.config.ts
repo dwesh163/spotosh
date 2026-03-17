56d8f4d0a7aa7c2c0edc54d44fdb794261c58cd6
dwesh163 <dwesh163@protonmail.com>
2026-03-17 12:37:25 +0100

    [feature] add Dockerfile for multi-stage build and configure next.config.ts for standalone output

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      { hostname: "is1-ssl.mzstatic.com" },
+      { hostname: "is2-ssl.mzstatic.com" },
+      { hostname: "is3-ssl.mzstatic.com" },
+      { hostname: "is4-ssl.mzstatic.com" },
+      { hostname: "is5-ssl.mzstatic.com" },
+    ],
+  },
 };
 
 export default nextConfig;
