f06c3d25aba0985de9a0bffb59f081539892dac3
dwesh163 <dwesh163@protonmail.com>
2026-03-17 12:44:42 +0100

    [feature] add constants and utility functions for genre presets and time formatting

---
+++ web/src/lib/constants.ts
@@ -0,0 +1,16 @@
+import type { GenrePreset } from "@/types/music";
+
+export const GENRE_PRESETS: GenrePreset[] = [
+  { label: "Rap FR", query: "rap français" },
+  { label: "Hip-Hop", query: "hip hop" },
+  { label: "Drill", query: "drill" },
+  { label: "Pop", query: "pop hits" },
+  { label: "R&B", query: "rnb soul" },
+  { label: "Rock", query: "rock" },
+  { label: "Électro", query: "electronic" },
+  { label: "Afrobeats", query: "afrobeats" },
+  { label: "Jazz", query: "jazz" },
+  { label: "Trap", query: "trap" },
+  { label: "Indie", query: "indie" },
+  { label: "Reggaeton", query: "reggaeton" },
+];
