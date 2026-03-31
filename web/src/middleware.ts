d1a1818dba400a97424158275c6bfdba072e34a9
dwesh163 <dwesh163@protonmail.com>
2026-03-31 14:38:23 +0200

    [feature] add middleware for session authentication; redirect to login if no session is found

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -5,7 +5,8 @@ export default async function proxy(req: NextRequest) {
   const { auth } = await import("./services/auth");
 
   const session = await auth();
-  if (!session) {
+  if (session === null) {
+    console.log("No session found, redirecting to login");
     const authUrl = new URL("/login", req.url);
     authUrl.searchParams.set("callbackUrl", req.url);
     return NextResponse.redirect(authUrl);
@@ -15,5 +16,5 @@ export default async function proxy(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!login|error|_next/static|_next/image|favicon.ico|.*\\.[a-zA-Z]+$).*)"],
+  matcher: ["/((?!login|api|error|_next/static|_next/image|favicon.ico|.*\\.[a-zA-Z]+$).*)"],
 };
